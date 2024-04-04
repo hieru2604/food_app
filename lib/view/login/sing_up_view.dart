86e13428e0c1e64e74b520b82a11f88912d93795
Hieru1650 <hieunguyenhuu2002@gmail.com>
2024-04-04 21:30:55 +0700

    Initial commit

---
+++ lib/view/login/sing_up_view.dart
@@ -0,0 +1,143 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:food_app/common/color_extension.dart';
+import 'package:food_app/common_widget/round_button.dart';
+import 'package:food_app/common_widget/round_icon_button.dart';
+import 'package:food_app/common_widget/round_textfield.dart';
+import 'package:food_app/view/login/login_view.dart';
+
+class SignUpView extends StatefulWidget {
+  const SignUpView({super.key});
+
+  @override
+  State<SignUpView> createState() => _SignUpViewState();
+}
+
+class _SignUpViewState extends State<SignUpView> {
+  TextEditingController txtName = TextEditingController();
+  TextEditingController txtMobile = TextEditingController();
+  TextEditingController txtAddress = TextEditingController();
+  TextEditingController txtEmail = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
+  TextEditingController txtConfirmPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              Text(
+                "Sign Up",
+                style: TextStyle(
+                    color: TColor.primaryText,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800),
+              ),
+              Text(
+                "Add your details to sign up",
+                style: TextStyle(
+                    color: TColor.secondaryText,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundTextfield(
+                hintText: "Name",
+                controller: txtName,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundTextfield(
+                hintText: "Email",
+                controller: txtEmail,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundTextfield(
+                hintText: "Mobile no",
+                controller: txtMobile,
+                keyboardType: TextInputType.phone,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundTextfield(
+                hintText: "Address",
+                controller: txtAddress,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundTextfield(
+                hintText: "Password",
+                controller: txtPassword,
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundTextfield(
+                hintText: "Confirm Password",
+                controller: txtConfirmPassword,
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundButton(
+                  title: "Sign Up",
+                  onPressed: () {
+                    //
+                  }),
+              
+              
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginView(),
+                    ),
+                  );
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Already have an Account?",
+                      style: TextStyle(
+                          color: TColor.secondaryText,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                          color: TColor.primary,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
